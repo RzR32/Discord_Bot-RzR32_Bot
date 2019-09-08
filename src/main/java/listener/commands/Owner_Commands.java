cfb388bd9d2e3acd168c568385ee26050c1ae6ff
RzR32 <46526880+RzR32@users.noreply.github.com>
2019-09-08 22:21:07 +0200

    added command to delete game
    gamerole, in the file and in the *#games* channel
    #8

--- src/main/java/listener/commands/Owner_Commands.java
+++ src/main/java/listener/commands/Owner_Commands.java
@@ -3,11 +3,15 @@ package listener.commands;
 import config.PropertiesFile;
 import net.dv8tion.jda.api.EmbedBuilder;
 import net.dv8tion.jda.api.entities.ChannelType;
+import net.dv8tion.jda.api.entities.Message;
+import net.dv8tion.jda.api.entities.Role;
 import net.dv8tion.jda.api.entities.TextChannel;
 import net.dv8tion.jda.api.events.message.MessageReceivedEvent;
 import net.dv8tion.jda.api.hooks.ListenerAdapter;
+import writeFile.RemoveStringFromFile;
 
 import java.awt.*;
+import java.util.ArrayList;
 
 public class Owner_Commands extends ListenerAdapter {
 
@@ -31,22 +35,14 @@ public class Owner_Commands extends ListenerAdapter {
                                     "> >twitch <name> (maybe don´t update the counter instantly)\n" +
                                     "\n" +
                                     "> >settings\n" +
+                                    "\n" +
+                                    "> >del <game>\n" +
                                     "\n").build()).queue();
                             event.getMessage().addReaction("\uD83D\uDC4D").queue();
                         } else {
                             event.getChannel().sendMessage("Dieser Befehl ist nur für den Server Besitzer!").queue();
                             event.getMessage().addReaction("\u274C").queue();
                         }
-                        //example
-                        /*
-                    } else if (argArray[0].equalsIgnoreCase(">help+")) {
-                        if (event.getGuild().getMember(event.getMember().getUser()).isOwner()) {
-                            //code
-                        } else {
-                            event.getChannel().sendMessage("Dieser Befehl ist nur für den Server Besitzer!").queue();
-                            event.getMessage().addReaction("\u274C").queue();
-                        }
-                         */
 
                         /*
                         set the twitchname
@@ -74,6 +70,60 @@ public class Owner_Commands extends ListenerAdapter {
                         } else {
                             event.getChannel().sendMessage("Dieser Befehl ist nur für den Server Besitzer!").queue();
                             event.getMessage().addReaction("\u274C").queue();
+                        }
+
+                        /*
+                        > remove game from file
+                        > delete the gamerole (if created)
+                        > delete the ONE message in #games
+                         */
+                    } else if (argArray[0].equalsIgnoreCase(">del")) {
+                        if (argArray[1] == null) {
+                            event.getChannel().sendMessage("Hey, you forgot the game name!").queue();
+                        } else {
+                            if (event.getGuild().getMember(event.getMember().getUser()).isOwner()) {
+                                ArrayList<String> list = new ArrayList<>();
+                                for (int x = 0; x < argArray.length; x++) {
+                                    list.add(argArray[x]);
+                                    if (list.size() == argArray.length) {
+                                        list.remove(argArray[0]);
+                                        if (list.isEmpty()) {
+                                            return;
+                                        }
+                                        /*
+                                        liststring = game
+                                         */
+                                        String liststring = String.join(" ", list);
+                                        /*
+                                        remove game from file
+                                         */
+                                        RemoveStringFromFile RSFF = new RemoveStringFromFile();
+                                        RSFF.remove(event.getGuild(), "games", liststring);
+                                        /*
+                                        delete the gamerole
+                                         */
+                                        for (Role role : event.getGuild().getRoles()) {
+                                            if (role.getName().equalsIgnoreCase(liststring)) {
+                                                role.delete().queue();
+                                                break;
+                                            }
+                                        }
+                                        /*
+                                        delete the ONE message in #games
+                                         */
+                                        for (Message message : event.getGuild().getTextChannelById(PropertiesFile.readsPropertiesFile("games")).getIterableHistory()) {
+                                                if (message.getEmbeds().get(0).getTitle().equalsIgnoreCase(liststring)) {
+                                                    message.delete().queue();
+                                                    break;
+                                                }
+                                        }
+                                        event.getMessage().addReaction("\uD83D\uDC4D").queue();
+                                    }
+                                }
+                            } else {
+                                event.getChannel().sendMessage("Dieser Befehl ist nur für den Server Besitzer!").queue();
+                                event.getMessage().addReaction("\u274C").queue();
+                            }
                         }
                     }
                 }
