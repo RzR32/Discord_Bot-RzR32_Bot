fc06a4821bf31be146e00e0f7014b3620cee76f8
RzR32 <46526880+RzR32@users.noreply.github.com>
2020-02-28 22:45:20 +0100

    Update BackUp.java
    Added some Structure like in Issues #16

--- src/main/java/other/BackUp.java
+++ src/main/java/other/BackUp.java
@@ -20,18 +20,24 @@ public class BackUp {
     private File file_temp = new File("backup_temp.zip");
     private File file_final = new File("backup_final.zip");
 
-    private SimpleDateFormat SDF = new SimpleDateFormat("YYYY-MM-DD_HH-mm");
+    private SimpleDateFormat SDF = new SimpleDateFormat("YYYY-MM-dd_HH-mm");
     private Date D = new Date();
     private String date = SDF.format(D);
-    private File backup = new File("backup/");
-    private File output = new File("backup/" + date + "/");
-    private File input = new File("config/");
+
+    private SimpleDateFormat SDF_m = new SimpleDateFormat("MM");
+    private Date D_m = new Date();
+    private String date_m = SDF_m.format(D_m);
+
+    private static ArrayList<File> list = new ArrayList<>() {{
+        add(new File("config/"));
+        add(new File("logs/"));
+    }};
 
     public void timer() {
         Runnable runnable = () -> {
             try {
-                checkoldbackup();
                 getFiles();
+                OldBackUp();
                 TimeUnit.HOURS.sleep(24);
             } catch (Exception e) {
                 LB.log(Thread.currentThread().getName(), e.getMessage(), "error");
@@ -41,35 +47,56 @@ public class BackUp {
     }
 
     private void getFiles() {
-        boolean bool_backup = backup.mkdir();
-        boolean bool_output = output.mkdir();
-        if (bool_output) {
-            LB.log(Thread.currentThread().getName(), "Create Backup *" + output.getAbsoluteFile().getName() + "*", "info");
-            for (File file : input.listFiles()) {
-                if (!file.getName().contains("png")) {
-                    if (file.isDirectory()) {
-                        File file_out = new File("backup/" + date + "/" + file.getName());
-                        boolean b = file_out.mkdir();
-                        if (b) {
-                            for (File file1 : file.listFiles()) {
-                                copyFile(file1, file1.getParentFile().getName());
+        try {
+            if (new File("backup/" + "/" + date + "/").exists()) {
+                return;
+            }
+
+            for (File file_list : list) {
+
+                File b_backup = new File("backup/");
+                boolean bool_backup = b_backup.mkdir();
+
+                File b_date = new File(b_backup + "/" + date + "/");
+                boolean bool_date = b_date.mkdir();
+
+                File b_folder = new File(b_date.getPath() + "/" + file_list + "/");
+                boolean bool_folder = b_folder.mkdir();
+
+                for (File file : file_list.listFiles()) {
+                    if (!file.getName().contains("png")) {
+                        if (file.isDirectory()) {
+                            File file_out = new File(b_folder + "/" + file.getName());
+                            boolean b = file_out.mkdir();
+                            if (b) {
+                                for (File file1 : file.listFiles()) {
+                                    copyFile(file1, "config", file1.getParentFile().getName());
+                                }
+                            }
+                        } else if (file.isFile()) {
+                            if (file.getName().contains(".log")) {
+                                if (file.getName().contains(date.substring(0, 10))) {
+                                    copyFile(file, "logs", null);
+                                }
+                            } else {
+                                copyFile(file, "config", null);
                             }
                         }
-                    } else if (file.isFile()) {
-                        copyFile(file, null);
                     }
                 }
             }
+        } catch (NullPointerException e) {
+            LB.log(Thread.currentThread().getName(), e.getMessage(), "error");
         }
     }
 
-    private void copyFile(File file, String Parent) {
+    private void copyFile(File file, String MainFolder, String Parent) {
         try {
             File file_out;
             if (Parent != null) {
-                file_out = new File("backup/" + date + "/" + Parent + "/" + file.getName());
+                file_out = new File("backup/" + date + "/" + MainFolder + "/" + Parent + "/" + file.getName());
             } else {
-                file_out = new File("backup/" + date + "/" + file.getName());
+                file_out = new File("backup/" + date + "/" + MainFolder + "/" + file.getName());
             }
             boolean b = file_out.createNewFile();
             if (b) {
@@ -87,55 +114,61 @@ public class BackUp {
         }
     }
 
-    private void checkoldbackup() throws Exception {
-        if (!file_original.exists()) {
-            file_original.createNewFile();
+    private void OldBackUp() throws Exception {
+        File backup = new File("backup/");
+        if (backup.listFiles().length > 0) {
+            boolean bool_backup = backup.mkdir();
+
+            int backup_count = 7;
+            int size = backup.listFiles().length;
+            int cal = size - backup_count;
+
+            if (cal < 1) {
+            } else {
+                if (!file_original.exists()) {
+                    file_original.createNewFile();
+                }
+                if (!file_temp.exists()) {
+                    file_temp.createNewFile();
+                }
+                if (!file_final.exists()) {
+                    file_final.createNewFile();
+                }
+
+                File[] file = new File("backup/").listFiles();
+
+                ArrayList<File> list = new ArrayList<>();
+                assert file != null;
+                Collections.addAll(list, file);
+                Collections.sort(list);
+
+                for (int x = 0; x < cal; x++) {
+                    int month_file = Integer.parseInt(list.get(x).getName().substring(5, 7));
+                    /*ZIP*/
+                    FileOutputStream fos = new FileOutputStream(file_temp.getName());
+                    ZipOutputStream zos = new ZipOutputStream(fos);
+                    zos.setMethod(ZipOutputStream.DEFLATED);
+                    zos.setLevel(5);
+
+                    ZIP_BackUp(zos, list.get(x), list.get(x).getParentFile().getName(), cal, month_file);
+
+                    zos.flush();
+                    fos.flush();
+                    zos.close();
+                    fos.close();
+                    /*DELETE*/
+                    deleteFolder(list.get(x));
+                }
+                boolean a = file_original.delete();
+                boolean b = file_temp.delete();
+                if (!file_original.exists() && !file_temp.exists()) {
+                    boolean c = file_final.renameTo(file_original);
+                }
+            }
         }
-        if (!file_temp.exists()) {
-            file_temp.createNewFile();
-        }
-        if (!file_final.exists()) {
-            file_final.createNewFile();
-        }
-
-        boolean bool_backup = backup.mkdir();
-
-        File[] file = new File("backup/").listFiles();
-
-        ArrayList<File> list = new ArrayList<>();
-        assert file != null;
-        Collections.addAll(list, file);
-        Collections.sort(list);
-
-        FileOutputStream fos = new FileOutputStream(file_temp.getName());
-        ZipOutputStream zos = new ZipOutputStream(fos);
-        zos.setMethod(ZipOutputStream.DEFLATED);
-        zos.setLevel(5);
-
-        int backup_count = 7;
-        int size = list.size();
-        int cal = size - backup_count + 1;
-
-        for (int x = 0; x < cal; x++) {
-            /*ZIP*/
-            Zip(zos, list.get(x), list.get(x).getParentFile().getName(), cal);
-            LB.log(Thread.currentThread().getName(), "ZIP Backup *" + list.get(x).getAbsoluteFile().getName() + "* to Backup.zip", "info");
-            /*DELETE*/
-            deleteFolder(list.get(x));
-        }
-        boolean a = file_temp.delete();
-        boolean b = file_original.delete();
-
-        if (!file_original.exists() && !file_temp.exists()) {
-            boolean c = file_final.renameTo(file_original);
-        }
-        zos.flush();
-        fos.flush();
-        zos.close();
-        fos.close();
     }
 
-    private void Zip(ZipOutputStream zos, File fileToZip, String parrentDirectoryName, int cal) {
+    private void ZIP_BackUp(ZipOutputStream zos, File fileToZip, String parrentDirectoryName, int cal, int month_file) {
         try {
             if (fileToZip == null || !fileToZip.exists()) {
                 return;
@@ -145,13 +178,16 @@ public class BackUp {
             if (parrentDirectoryName != null && !parrentDirectoryName.isEmpty()) {
                 if (!parrentDirectoryName.equals("backup")) {
                     zipEntryName = parrentDirectoryName + "/" + fileToZip.getName();
+                } else {
+                    zipEntryName = month(month_file) + "/" + fileToZip.getName();
                 }
             }
 
             if (fileToZip.isDirectory()) {
                 for (File file : fileToZip.listFiles()) {
-                    Zip(zos, file, zipEntryName, cal);
+                    ZIP_BackUp(zos, file, zipEntryName, cal, month_file);
                 }
+
             } else {
                 byte[] buffer = new byte[1024];
                 FileInputStream fis = new FileInputStream(fileToZip);
@@ -168,12 +204,11 @@ public class BackUp {
                 ZipOutputStream outStream = new ZipOutputStream(new FileOutputStream(file_final));
                 readZip(outStream, file_original.getName());
                 readZip(outStream, file_temp.getName());
+                outStream.flush();
                 outStream.close();
             }
-        } catch (IOException e) {
-            LB.log(Thread.currentThread().getName(), e.getMessage(), "error");
         } catch (Exception e) {
-            e.printStackTrace();
+            LB.log(Thread.currentThread().getName(), e.getMessage(), "error");
         }
     }
 
@@ -190,9 +225,9 @@ public class BackUp {
                     outStream.write(buffer, 0, len);
                 }
             }
-            inStream.closeEntry();
+            inStream.close();
         } catch (Exception e) {
-            e.printStackTrace();
+            System.err.println(e.getMessage());
         }
     }
 
@@ -208,5 +243,35 @@ public class BackUp {
             }
         }
         folder.delete();
+    }
+
+    static String month(int month_file) {
+        if (month_file == 1) {
+            return "January";
+        } else if (month_file == 2) {
+            return "February";
+        } else if (month_file == 3) {
+            return "March";
+        } else if (month_file == 4) {
+            return "April";
+        } else if (month_file == 5) {
+            return "May";
+        } else if (month_file == 6) {
+            return "June";
+        } else if (month_file == 7) {
+            return "July";
+        } else if (month_file == 8) {
+            return "August";
+        } else if (month_file == 9) {
+            return "September";
+        } else if (month_file == 10) {
+            return "October";
+        } else if (month_file == 11) {
+            return "November";
+        } else if (month_file == 12) {
+            return "December";
+        } else {
+            return "error";
+        }
     }
 }
