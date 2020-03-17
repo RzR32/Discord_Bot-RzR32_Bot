28a920a896741b230cc1fd9921849fcab47e4539
RzR32 <46526880+RzR32@users.noreply.github.com>
2020-03-17 21:18:48 +0100

    downgrade JDA Version
    JDA:4.1.1_111 and above dont work for me

--- src/main/java/listener/member/Member_join_leave.java
+++ src/main/java/listener/member/Member_join_leave.java
@@ -3,15 +3,16 @@ package listener.member;
 import config.PropertiesFile;
 import count.Counter;
 import net.dv8tion.jda.api.events.guild.member.GuildMemberJoinEvent;
-import net.dv8tion.jda.api.events.guild.member.GuildMemberRemoveEvent;
+import net.dv8tion.jda.api.events.guild.member.GuildMemberLeaveEvent;
 import net.dv8tion.jda.api.hooks.ListenerAdapter;
+
 
 public class Member_join_leave extends ListenerAdapter {
 
     Counter c = new Counter();
 
     @Override
-    public void onGuildMemberRemove(GuildMemberRemoveEvent event) {
+    public void onGuildMemberLeave(GuildMemberLeaveEvent event) {
         if (PropertiesFile.readsPropertiesFile("first-startup").equals("false")) {
             c.getint(event.getGuild(), "membercount");
         }
